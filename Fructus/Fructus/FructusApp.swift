567ec5e851788c3dc8bfa133b95819a8150ff48d
Pekomon <5183385+pekomon@users.noreply.github.com>
2023-04-04 08:57:08 +0300

    Empty default project

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Pekka Rasanen on 4.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
