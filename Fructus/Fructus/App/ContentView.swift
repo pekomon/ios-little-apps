34e651f8da13cf28b6dffd2ada2c0e0c44e6846a
Pekomon <5183385+pekomon@users.noreply.github.com>
2023-04-14 10:02:46 +0300

    Components for settings view

--- Fructus/Fructus/App/ContentView.swift
+++ Fructus/Fructus/App/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
 
     // MARK: - PROPERTIES
+    
+    @State private var isShowningSettings: Bool = false
+    
     var fruits: [Fruit] = fruitsData
     
     // MARK: - BO
@@ -24,6 +27,17 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Fruits")
+            .navigationBarItems(
+                trailing:
+                    Button(action: {
+                        isShowningSettings = true
+                    }) {
+                        Image(systemName: "slider.horizontal.3")
+                    } //: BUTTON
+                    .sheet(isPresented: $isShowningSettings) {
+                        SettingsView()
+                    }
+            )
         } //: NAVIGATION
     }
 }
---
+++ Fructus/Fructus/View/SettingsLabelView.swift
@@ -0,0 +1,34 @@
+//
+//  SEttingsLabelView.swift
+//  Fructus
+//
+//  Created by Pekka Rasanen on 13.4.2023.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    // MARK: - PROPERTIES
+    
+    var labelText: String
+    var labelImage: String
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased()).fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        }
+    }
+}
+
+// MARK: - PREVIEW
+
+struct SEttingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
