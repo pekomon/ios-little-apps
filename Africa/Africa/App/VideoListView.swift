2c0e0eff960251401cc4c4c4d9b393da9eb14d15
Pekomon <5183385+pekomon@users.noreply.github.com>
2023-04-17 13:45:09 +0300

    Navigation and placeholders for feature views

---
+++ Africa/Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Pekka Rasanen on 17.4.2023.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
