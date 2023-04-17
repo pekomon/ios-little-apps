1e4304da3815d820c7e8739624ed317bff28b6a1
Pekomon <5183385+pekomon@users.noreply.github.com>
2023-04-17 17:42:58 +0300

    Populate list of animals from json to ContentView

--- Africa/Africa/App/ContentView.swift
+++ Africa/Africa/App/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         // MARK: - PROPERTIES
         
+        let animals: [Animal] = Bundle.main.decode("animals.json")
+        
         // MARK: - BODY
         NavigationView {
             List {
@@ -19,6 +21,9 @@ struct ContentView: View {
                     .listRowInsets(
                         EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
                     )
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
             } //: LIST
             .listStyle(PlainListStyle())
             .navigationBarTitle("Africa", displayMode: .large)
