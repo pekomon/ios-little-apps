9fa78caae0abf346023fc45e8773df23223372b0
Pekomon <5183385+pekomon@users.noreply.github.com>
2023-04-17 13:01:07 +0300

    Setup empty project

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Pekka Rasanen on 17.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
