dc675c9580226962b91017f7c117deb27ca16d5b
Pekomon <5183385+pekomon@users.noreply.github.com>
2023-04-19 11:22:04 +0300

    Locations view with placeholder map

--- Africa/Africa/App/MapView.swift
+++ Africa/Africa/App/MapView.swift
@@ -6,10 +6,44 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    } ()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     var body: some View {
-        Text("Map")
+        // Basic map
+        // Map(coordinateRegion: $region)
+        
+        // Advanced map
+        Map(
+            coordinateRegion: $region,
+            annotationItems: locations,
+            annotationContent: { item in
+                // a) PIN - OLD STYLE, always static
+                //MapPin(coordinate: item.location, tint: .accentColor)
+                
+                // b) MARKER - NEW Style, always static
+                //MapMarker(coordinate: item.location, tint: .accentColor)
+                
+                // c) CUSTOM basic annotation (can be interactive)
+                MapAnnotation(coordinate: item.location) {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32, alignment: .center)
+                } //: ANNOTATION
+            }
+        )
+        
     }
 }
 
