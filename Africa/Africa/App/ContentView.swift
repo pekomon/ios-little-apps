9d59553ad9b18323c98ab27dea53c852aaecac17
Pekomon <5183385+pekomon@users.noreply.github.com>
2023-04-20 10:32:21 +0300

    Buttons to toolbar of ContentView

--- Africa/Africa/App/ContentView.swift
+++ Africa/Africa/App/ContentView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridViewActive: Bool = false
+    
     var body: some View {
-        // MARK: - PROPERTIES
-        
-        let animals: [Animal] = Bundle.main.decode("animals.json")
         
         // MARK: - BODY
         NavigationView {
@@ -30,6 +33,33 @@ struct ContentView: View {
             } //: LIST
             .listStyle(PlainListStyle())
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // LIST
+                        Button(action: {
+                            print("List View activated")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        
+                        // GRID
+                        Button(action: {
+                            print("Grid view activated")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    } //: HSTACK
+                } //: BUTTONS
+            } //: TOOLBAR
         } //: NAVIGATION
     }
 }
