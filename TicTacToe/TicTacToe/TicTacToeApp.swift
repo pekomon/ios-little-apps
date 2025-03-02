dcc4d8f70b625a27cdaa228b4fd8163cf399f0b4
Pekomon <5183385+pekomon@users.noreply.github.com>
2025-03-02 16:04:02 +0200

    Initial commit: empty Xcode project

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Pekka Rasanen on 2.3.2025.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicTacToe/TicTacToeTests/TicTacToeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeTests.swift
+//  TicTacToeTests
+//
+//  Created by Pekka Rasanen on 2.3.2025.
+//
+
+import Testing
+@testable import TicTacToe
+
+struct TicTacToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
