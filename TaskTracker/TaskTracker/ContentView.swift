d7fbd26cb84daebb8e176cb89db2f6152b55971f
Pekomon <5183385+pekomon@users.noreply.github.com>
2026-03-07 09:30:20 +0200

    Empty skeleton project for TaskTracker

---
+++ TaskTracker/TaskTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskTracker
+//
+//  Created by Pekomon on 7.3.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Tasks!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskTracker/TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by Pekomon on 7.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
