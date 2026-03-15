7d1a4b7bd89b03810cd5367a91382dc4783cad60
Pekomon <5183385+pekomon@users.noreply.github.com>
2026-03-15 19:51:08 +0200

    Add project folder structure

--- Weatherly/Weatherly/App/RootView.swift
+++ Weatherly/Weatherly/App/RootView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RootView.swift
 //  Weatherly
 //
 //  Created by Pekomon on 15.3.2026.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    RootView()
 }
