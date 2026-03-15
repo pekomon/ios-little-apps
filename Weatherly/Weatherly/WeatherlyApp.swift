a7654645f76375820fe63cbc9767d9a1de59de9d
Pekomon <5183385+pekomon@users.noreply.github.com>
2026-03-15 19:34:43 +0200

    Initialize Weatherly SwiftUI project

---
+++ Weatherly/Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Pekomon on 15.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weatherly/WeatherlyTests/WeatherlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyTests.swift
+//  WeatherlyTests
+//
+//  Created by Pekomon on 15.3.2026.
+//
+
+import Testing
+@testable import Weatherly
+
+struct WeatherlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
