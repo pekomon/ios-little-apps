af97db14c791ed4ee7bfaf25bdd526e5a75835ad
Pekomon <5183385+pekomon@users.noreply.github.com>
2026-04-14 18:53:05 +0300

    Initialize LockBox SwiftUI project

---
+++ LockBox/LockBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LockBox
+//
+//  Created by Pekomon on 14.4.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, LockBox!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LockBox/LockBox/LockBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockBoxApp.swift
+//  LockBox
+//
+//  Created by Pekomon on 14.4.2026.
+//
+
+import SwiftUI
+
+@main
+struct LockBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
